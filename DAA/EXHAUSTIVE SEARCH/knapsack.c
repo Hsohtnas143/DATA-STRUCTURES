a1cf12d3fa18cba50d0ed12588c305a976eb55ae
Hsohtnas143 <78403321+Hsohtnas143@users.noreply.github.com>
2021-10-23 02:15:41 +0530

    Update knapsack.c

--- DAA/EXHAUSTIVE SEARCH/knapsack.c
+++ DAA/EXHAUSTIVE SEARCH/knapsack.c
@@ -1,171 +1,61 @@
 #include<stdio.h>
 #include<math.h>
-struct items
+int items[100];
+int max_profit = 0;
+int count1 = 0;
+void powerSet(int p_w[][2],int noOfItems,int max_weight)
 {
-    int item_number;
-    int value;
-    int weight;
-};
-
-struct table
-{
-    int item_numbers[100];
-    int length;
-    int values;
-    int weights;
-};
-int count=0;
-
-void combinations(struct items a[], int print[], int start, int end, int index, int r,struct table t[])
-{
-    if(index==r)
+    int sum;
+    int weight = max_weight;
+    int arr[noOfItems];
+    int count = 0;
+    for(int i=1;i<pow(2,noOfItems);i++)
     {
-        int val=0;
-        int weigh = 0;
-        if(index==0)
+        sum = 0;
+        weight = max_weight;
+        count = 0;
+        for(int j=0;j<noOfItems;j++)
         {
-            t[count].item_numbers[0] = 0;
-            t[count].weights = 0;
-            t[count].values = 0;
-            //printf("ϕ");
-            count++;
-            return;
+            if((i & (1<<j)) && (weight>=p_w[j][1]))
+            {
+                sum+= p_w[j][0];
+                printf("\nmax weight %d\n",weight);
+                weight-=p_w[j][1];
+                arr[count++] = j;
+            }
         }
-        for(int j=0;j<r;j++)
+        if(sum>max_profit)
         {
-            t[count].item_numbers[j] = print[j];
-            val+=a[print[j]-1].value;
-            weigh+=a[print[j]-1].weight;
-            //printf("%d",print[j]);
-        }
-        t[count].values = val;
-        t[count].weights = weigh;
-        t[count].length = r;
-        count++;
-        return;
-    }
-    else
-    {
-        for(int i = start; i<=end && end-i+1>= r-index;i++)
-        {
-            print[index] = a[i].item_number;
-            combinations(a,print,i+1,end,index+1,r,t);
+            max_profit = sum;
+            count1 = count;
+            for(int k=0;k<count;k++)
+            {
+                items[k] = arr[k];
+            }
         }
     }
 }
-
-void subsets(struct items I[], int length,struct table t[],int n)
+int main()
 {
-    
-    for(int i=0;i<=length;i++)
+    int noOfItems;
+    printf("\nEnter the noOfItems : ");
+    scanf("%d",&noOfItems);
+    int p_w[noOfItems][2];
+    for(int i=0;i<noOfItems;i++)
     {
-        int print[i+1];
-        combinations(I,print,0,length-1,0,i,t);
+        printf("\nEnter the profit %d : ",i);
+        scanf("%d",&p_w[i][0]);
+        printf("\nEnter the weight : ");
+        scanf("%d",&p_w[i][1]);
     }
-    
-}
-
-
-void print_all(struct table t[], int length, int decider, int W)
-{
-    for(int i=0;i<length;i++)
+    int max_weight;
+    printf("\nEnter the max weight : ");
+    scanf("%d",&max_weight);
+    powerSet(p_w,noOfItems,max_weight);
+    for(int i=0;i<count1;i++)
     {
-        printf("\n\n pairs : {");
-        for(int j=0;j<t[i].length;j++)
-        {
-            printf("%d",t[i].item_numbers[j]);
-            if(!(j+1==t[i].length))
-                printf(", ");
-                
-        }
-        if(i==0)
-        {
-            printf("ϕ");
-        }
-        printf("}\n weights : %d",t[i].weights);
-        if(decider==1 && W<t[i].weights)
-            printf("\n not feasible");
-        else
-            printf("\n values : $%d ",t[i].values);
-        
-        
+        printf("%d ",items[i]);
     }
-}
-
-void print_max(struct table t[], int length, int W)
-{
-    int val = 0,index=-1;
-    for(int i=0;i<length;i++)
-    {
-        if(t[i].weights <= W && t[i].values >=val)
-        {
-            index = i;
-            val = t[i].values;
-        }
-    }
-    printf("\n pairs : {");
-    for(int j=0;j<t[index].length;j++)
-    {
-        printf("%d",t[index].item_numbers[j]);
-        if(!(j+1==t[index].length))
-            printf(", ");
-    }
-    if(index!=-1)
-        printf("}\n\n the maximum value: $%d \nweigth is %d ",t[index].values,t[index].weights);
-    else
-        printf("\n not exits");
-}
-
-void Knapsack(struct items I[], int n, int W)
-{
-    int length = pow(2,n);
-    struct table t[length];
-    subsets(I,n,t,length);
-    int ch,i,j=1;
-    while(j)
-    {
-        printf("\n\n 1.print all possibilities\n2.print maximum feasible value\n3.print feasible values\npress any key to exit\n");
-        scanf("%d",&ch);
-        switch(ch)
-        {
-            case 1:
-                i=0;
-                print_all(t,length,i,W);
-                break;
-            case 2:
-                print_max(t,length,W);
-                break;
-            case 3:
-                i=1;
-                print_all(t,length,i,W);
-                break;
-            default:
-                j=0;
-                break;
-        }
-    }
-    
-    
-    
-}
-
-void main()
-{
-    printf("\nenter the number of items : ");
-    int n;
-    scanf("%d",&n);
-    struct items I[n];
-    for(int i=0;i<n;i++)
-    {
-        I[i].item_number = i+1;
-        printf("\n enter the value of item_number %d : ",i+1);
-        scanf("%d",&I[i].value);
-        printf("\n weight of item_number %d : ",i+1);
-        scanf("%d",&I[i].weight);
-    }
-    printf("\n\n PROVIDE THE WEIGHT : ");
-    int W;
-    scanf("%d",&W);
-    Knapsack(I,n,W);
-    
+    printf("\n profit : %d",max_profit);
+    return 0;
 }
